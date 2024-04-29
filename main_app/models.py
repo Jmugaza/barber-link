6b455b60a4d72dd2c9c50ba7dc9ac296775f021c
jeilanim <jeilandahiri2@gmail.com>
2024-04-29 16:17:30 -0400

    adding auth and nav

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Barber(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    bio = models.TextField(max_length=250)
+
+    def __str__(self) :
+        return self.name
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def about(request):
+  return render(request, 'about.html')
